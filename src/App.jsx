e6a04b6c292b90301a7d523fb91b13e0fd3b1c32
Boobies <dmychel@tutanota.com>
2025-07-31 21:54:40 -0400

    #Card Logic
    Created basic logic to split deck in half and assign to both players ibject property. This is done by manipulating a copy of the main decks array. I wrote a function to shuffle the array but I ran into a problem of the result getting infinetely nested..
    
    WIll come back to the shuffle feature when games foundation has been more established.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react';
 import './App.css'
+import { playerOne, playerTwo } from '/src/functions/players.js'
+import { initiateCardLogic } from './functions/cardLogic'
 
 function App() {
+
+  useEffect(() => {
+    initiateCardLogic();
+    console.log('running on load')
+  }, [])
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+      <div>
+        <p>{playerOne.name} has {playerOne.cards.length} cards remaning</p>
+      </div>
+      <div>{playerTwo.name} has {playerTwo.cards.length} cards remaining</div>
+    </>
   )
 }
 
